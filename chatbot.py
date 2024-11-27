6ddbb19f52c706ef35a2d937a6b2740fd1303519
blister-pack <andrecrujo@hotmail.com>
2024-11-27 23:44:22 +0100

    created root for fastapi project and weekdays program

--- chatbot.py
+++ chatbot.py
@@ -11,3 +11,7 @@ TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 MY_PHONE_NUMBER = os.getenv("MY_PHONE_NUMBER")
+
+@app.get("/")
+def index():
+    return {"hello":"world"}
---
+++ thirdfriday.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+
+weekdays = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+]
+# this function returns the third friday of the month
+
+
+# this function returns what day of the week is today
+def today():
+    day_today = datetime.now().weekday()
+    return weekdays[day_today]
+
+def tomorrow():
+    day_tomorrow = datetime.now().weekday() + 1
+    return weekdays[day_tomorrow]
+
+if __name__ == "__main__":
+    print(today())
+    print(tomorrow())
