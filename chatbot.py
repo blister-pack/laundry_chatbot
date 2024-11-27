f217dbcb3c9bbb853a4206140098725305dc90ba
blister-pack <andrecrujo@hotmail.com>
2024-11-27 22:43:21 +0100

    environment variables created safely

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI, Path
+from twilio.twiml.messaging_response import MessagingResponse
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+MY_PHONE_NUMBER = os.getenv("MY_PHONE_NUMBER")
