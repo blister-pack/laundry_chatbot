b82f4889c0dca87898e6e4bb755d69ea9d097e7f
blister-pack <andrecrujo@hotmail.com>
2024-11-28 13:09:21 +0100

    reorganized test functions

--- thirdfriday.py
+++ thirdfriday.py
@@ -9,13 +9,6 @@ weekdays = [
     "saturday",
     "sunday",
 ]
-# this function returns the third friday of the month
-def get_third_friday(year, month):
-    first_day = datetime(year, month, 1)
-    days_until_friday = (4 - first_day.weekday() + 7) % 7
-    first_friday = first_day + timedelta(days=days_until_friday)
-    third_friday = first_friday + timedelta(days=14)
-    return third_friday
 
 # this function returns what day of the week is today
 def today():
@@ -25,6 +18,14 @@ def today():
 def tomorrow():
     day_tomorrow = datetime.now().weekday() + 1
     return weekdays[day_tomorrow]
+
+# this function returns the third friday of the month
+def get_third_friday(year, month):
+    first_day = datetime(year, month, 1)
+    days_until_friday = (4 - first_day.weekday() + 7) % 7
+    first_friday = first_day + timedelta(days=days_until_friday)
+    third_friday = first_friday + timedelta(days=14)
+    return third_friday
 
 if __name__ == "__main__":
     print(today())
