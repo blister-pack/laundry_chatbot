6e2980ca60174e7af85f5126c7c2e490a741d7a9
blister-pack <andrecrujo@hotmail.com>
2024-11-28 13:08:44 +0100

    created function that returns the third friday of the month, given the year

--- thirdfriday.py
+++ thirdfriday.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 weekdays = [
     "monday",
@@ -10,7 +10,12 @@ weekdays = [
     "sunday",
 ]
 # this function returns the third friday of the month
-
+def get_third_friday(year, month):
+    first_day = datetime(year, month, 1)
+    days_until_friday = (4 - first_day.weekday() + 7) % 7
+    first_friday = first_day + timedelta(days=days_until_friday)
+    third_friday = first_friday + timedelta(days=14)
+    return third_friday
 
 # this function returns what day of the week is today
 def today():
