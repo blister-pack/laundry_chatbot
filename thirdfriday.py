4d784077a66edacd5c5273c7e4a849ffcf49512e
blister-pack <andrecrujo@hotmail.com>
2024-11-28 14:30:01 +0100

    built function that returns all third fridays of a given year

--- thirdfriday.py
+++ thirdfriday.py
@@ -10,14 +10,32 @@ weekdays = [
     "sunday",
 ]
 
+month_mapping = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
+
+
 # this function returns what day of the week is today
 def today():
     day_today = datetime.now().weekday()
     return weekdays[day_today]
 
+
 def tomorrow():
     day_tomorrow = datetime.now().weekday() + 1
     return weekdays[day_tomorrow]
+
 
 # this function returns the third friday of the month
 def get_third_friday(year, month):
@@ -27,6 +45,20 @@ def get_third_friday(year, month):
     third_friday = first_friday + timedelta(days=14)
     return third_friday
 
+
+def get_all_third_fridays():
+    all_third_fridays = [{"month": "day"}]
+    current_year = datetime.now().year
+
+    for month in range(1, 13):
+        third_friday = get_third_friday(current_year, month)
+        all_third_fridays.append({f"{month_mapping[month]}": f"{third_friday}"})
+
+    return all_third_fridays
+
+
 if __name__ == "__main__":
     print(today())
     print(tomorrow())
+    print(get_third_friday(2024, 11))
+    print(get_all_third_fridays())
