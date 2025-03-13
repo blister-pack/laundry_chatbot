eae8caa1190b674d3297efe82c651dd2a521d289
blister-pack <andrecrujo@hotmail.com>
2025-03-13 17:59:41 +0100

    refactored business logic, advancements in test

--- source/business_logic.py
+++ source/business_logic.py
@@ -45,35 +45,35 @@ def get_third_friday(year, month):
     days_until_friday = (4 - first_day_of_the_month.weekday() + 7) % 7
     first_friday = first_day_of_the_month + timedelta(days=days_until_friday)
     third_friday = first_friday + timedelta(days=14)
-    return third_friday
+    return third_friday.strftime("%Y-%m-%d")
 
 
-def get_all_third_fridays():
-    current_year = datetime.now().year
+def get_all_third_fridays(year_to_check=datetime.now().year):
 
     for month in range(1, 13):
         """we use formatted date to remove the timestamp"""
-        third_friday = get_third_friday(current_year, month)
-        formatted_date = third_friday.strftime("%Y-%m-%d")
-        all_third_fridays[f"{month_mapping[month]}"] = f"{formatted_date}"
+        third_friday = get_third_friday(year_to_check, month)
+        all_third_fridays[f"{month_mapping[month]}"] = f"{third_friday}"
 
     return all_third_fridays
 
 
-def is_right_week(month_to_check=datetime.now().month, current_day=datetime.now().day):
-    this_month = month_mapping[month_to_check]
+def is_right_week(
+    year_to_check=datetime.now().year,
+    month_to_check=datetime.now().month,
+    day_to_check=datetime.now().day,
+):
     # title because data is saved with a capitalized month
     # only need 2 last chars because it's the day (2024-02-16)
     # gotta turn it into an int so I can do math with it
-    sixth_friday_of_the_month = int(all_third_fridays[this_month.title()][-2::])
+    third_friday_month = get_third_friday(year_to_check, month_to_check).split("-")[2]
 
     """if the difference between the now and the third friday of
     the month is 4 then it is monday of the same week, if it's 0
     then it is friday!"""
-    if 0 <= sixth_friday_of_the_month - current_day <= 4:
+    if 0 <= int(third_friday_month) - day_to_check <= 4:
         return True
     return False
-
 
 
 if __name__ == "__main__":
@@ -83,4 +83,4 @@ if __name__ == "__main__":
     print(get_all_third_fridays())
     print(all_third_fridays["January"])
     print(int(all_third_fridays["january".title()][-2::]))
-    print(is_right_week(1, 14))
+    print(is_right_week(2025, 1, 14))
--- tests/test_is_right_week.py
+++ tests/test_is_right_week.py
@@ -1,8 +1,14 @@
 import pytest
 from source.business_logic import is_right_week
+import datetime
+
+@pytest.mark.parametrize(
+        "month, day, expected_result",
+        [
+            
+        ]
+)
 
 
-
-
-def test_return_right_week(month_to_check, current_day, expected_result):
+def test_return_right_week(month_to_check, day_to_check, expected_result):
     pass
